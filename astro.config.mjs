607316435da8df5edca15a409e0d21b12607cd7a
Danny Solivan <dark@nightconcept.net>
2025-04-05 20:33:55 -0700

    WIP, working state

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
