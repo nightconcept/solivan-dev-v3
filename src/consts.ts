b80995a457dae376edb6dbf28a6e76c0dfaeb150
Danny Solivan <dark@nightconcept.net>
2025-04-05 20:54:12 -0700

    WIP

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "/home/danny";
+export const SITE_DESCRIPTION = "Personal blog and website";
 export const SITE_HOME_POSTS_PER_PAGE = 5;
